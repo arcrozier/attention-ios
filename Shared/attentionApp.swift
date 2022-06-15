94ac61311e21fca69ed1d69cabb80360c983bf84
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2022-06-15 11:22:13 -0700

    Initial Commit

---
+++ Shared/attentionApp.swift
@@ -0,0 +1,17 @@
+//
+//  attentionApp.swift
+//  Shared
+//
+//  Created by Aragorn Crozier on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct attentionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
